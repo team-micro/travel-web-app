acbf243a07a6c8f49843d630cb5e8eb5fc98999c
Lisa Cantrell <lcantr7@wgu.edu>
2021-11-29 19:49:56 -0700

    implement recommendationNotFoundException

--- RecommendService/src/main/java/com/hcl/recommendservice/exception/RecommendationNotFoundException.java
+++ RecommendService/src/main/java/com/hcl/recommendservice/exception/RecommendationNotFoundException.java
@@ -1,4 +1,8 @@
 package com.hcl.recommendservice.exception;
 
-public class RecommendationNotFoundException {
+public class RecommendationNotFoundException extends Exception {
+    private long recommendation_id;
+    public RecommendationNotFoundException(long recommendation_id) {
+        super(String.format("Recommendation not found with id: '%s'", recommendation_id));
+    }
 }
