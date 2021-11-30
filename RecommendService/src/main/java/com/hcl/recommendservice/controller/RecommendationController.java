097160ae17129ce78e22b4256466459822a95f08
Lisa Cantrell <lcantr7@wgu.edu>
2021-11-29 20:59:56 -0700

    added some basic logging

--- RecommendService/src/main/java/com/hcl/recommendservice/controller/RecommendationController.java
+++ RecommendService/src/main/java/com/hcl/recommendservice/controller/RecommendationController.java
@@ -3,6 +3,8 @@ package com.hcl.recommendservice.controller;
 import com.hcl.recommendservice.exception.RecommendationNotFoundException;
 import com.hcl.recommendservice.model.Recommendation;
 import com.hcl.recommendservice.repository.RecommendationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +15,35 @@ import java.util.List;
 
 @RestController
 public class RecommendationController {
+    Logger logger = LoggerFactory.getLogger(RecommendationController.class);
 
     @Autowired
     RecommendationRepository recommendationRepository;
 
+    @RequestMapping("/")
+    public String Index() {
+        logger.trace("Home method accessed");
+        return "Recommendation Page";
+    }
+
     //Get all recommendations
     @GetMapping("/recommendations")
-    public List<Recommendation> getAllRecommendations() { return recommendationRepository.findAll();}
+    public List<Recommendation> getAllRecommendations() {
+        logger.trace("get-all method accessed");
+        try {
+            return recommendationRepository.findAll();
+        } catch (Exception e) {
+            logger.trace("Failed to get list of recommendations, see exception log");
+            throw e;
+        }
+    }
 
     //Get single recommendation by Id
+    //troubleshoot better logging for this request, or just switch to Evan's implementation
     @GetMapping("/recommendations/{id}")
     public Recommendation getRecommendationById(@PathVariable(value = "id") Long recommendationId)
         throws RecommendationNotFoundException {
+        logger.trace("get-by-id method accessed");
         return recommendationRepository.findById(recommendationId)
                 .orElseThrow(() -> new RecommendationNotFoundException(recommendationId));
     }
@@ -32,13 +51,15 @@ public class RecommendationController {
     //Create a recommendation
     @PostMapping("/recommendations")
     public Recommendation createRecommendation(@Valid @RequestBody Recommendation recommendation) {
-        return recommendationRepository.save(recommendation);
+            logger.trace("Add method accessed");
+            return recommendationRepository.save(recommendation);
     }
 
     //Update recommendation
     @PutMapping("/recommendations/{id}")
     public Recommendation updateRecommendationContent(@PathVariable(value = "id") Long recommendationId,
                                                       @Valid @RequestBody Recommendation recDetails) throws RecommendationNotFoundException {
+        logger.trace("update-by-id method accessed");
         Recommendation recommendation = recommendationRepository.findById(recommendationId)
                 .orElseThrow(() -> new RecommendationNotFoundException(recommendationId));
 
@@ -47,6 +68,7 @@ public class RecommendationController {
         recommendation.setRate(recDetails.getRate());
 
         Recommendation updatedRecommendation = recommendationRepository.save(recommendation);
+        logger.trace("Successfully updated recommendation #: " + recommendationId);
 
         return updatedRecommendation;
 
