395cfef7da0052000b93e0f9b1f9f19ddb8bb985
Evan D <evan.delasota@gmail.com>
2021-12-01 19:52:20 -0800

    Added service discovery dependency, enabled eureka client to service application

--- ReviewService/src/main/java/com/hcl/reviewservice/ReviewServiceApplication.java
+++ ReviewService/src/main/java/com/hcl/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,9 @@ package com.hcl.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class ReviewServiceApplication {
     public static void main(String[] args) {
