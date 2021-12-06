f0fa8680b3e12ef6d027936e02c7661639b1dc47
Henry Lao <hclao1995@gmail.com>
2021-12-06 03:35:06 -0800

    Add missing Eureka module files

---
+++ EurekaServiceRegistry/src/test/java/com/hcl/eurekaserviceregistry/ServiceRegistrationDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.eurekaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
