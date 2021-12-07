d0986f786e54a42332973cbc3d291fd5a7ccdabe
Henry Lao <hclao1995@gmail.com>
2021-12-06 22:26:30 -0800

    Add documentation

--- UserService/.sh/startup.sh
+++ UserService/.sh/startup.sh
@@ -1,2 +1,9 @@
-docker pull henrylao/user-service:latest
+# !/usr/bin/bash
+
+# Simple bash script to pull latest 
+# built user-service image from DockerHub
+# and deploy in container
+
+#docker pull henrylao/user-service:latest
+
 docker run henrylao/user-service:latest
