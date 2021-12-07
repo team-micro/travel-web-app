e4bb530b4324e0b634358c6a9c4119eb35dc433a
Henry Lao <hclao1995@gmail.com>
2021-12-06 22:16:36 -0800

    Add documentation

--- UserService/build.sh
+++ UserService/build.sh
@@ -1,9 +1,10 @@
+# 1. Run Maven commands by selecting from the following
 # Run with testing
 mvn clean && mvn package
 
 # Clean and run w/o testing
 #mvn clean && mvn package -Dmaven.test.failure.ignore=true -Dmaven.test.skip=true
 
-# Build image and test run in container
+# 2. Build image and test run in container
 docker build -t henrylao/user-service:latest .
 docker push henrylao/user-service:latest
