fa9ddc1e94641fd5a0d66059ed1e35da88f0df25
Henry Lao <hclao1995@gmail.com>
2021-12-06 22:27:09 -0800

    Add documentation

--- UserService/.sh/clean.sh
+++ UserService/.sh/clean.sh
@@ -1,3 +1,14 @@
+# !/usr/bin/bash
+
+##############################################
+# ---------------
+# -- IMPORTANT --
+# ---------------
+# This script will remove all local images and 
+# containerized apps on the local machine
+#
+##############################################
+
 # remove all containers
 docker rm $(docker ps -aq)
 
